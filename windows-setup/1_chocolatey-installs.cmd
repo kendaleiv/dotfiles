1da851756ffc9fe84cb7e23d6aa8f0cc1379ecfd
Ken Dale <ken@kendaleiv.com>
2015-03-27 17:45:01 -0400

    Remove thunderbird from windows-setup packages

--- windows-setup/1_chocolatey-installs.cmd
+++ windows-setup/1_chocolatey-installs.cmd
@@ -44,7 +44,6 @@ screentogif                            ^
 skype                                  ^
 spotify                                ^
 sysinternals                           ^
-thunderbird                            ^
 vlc                                    ^
 wcat                                   ^
 windirstat                             ^
