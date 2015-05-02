81cd1f2eeba7a0d8d8ef6f2da7b7d13c0da1988e
Ken Dale <ken@kendaleiv.com>
2015-05-02 13:55:45 -0400

    Add Chocolatey installation to windows-setup

--- windows-setup/chocolatey-installs.cmd
+++ windows-setup/chocolatey-installs.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-:: Install Chocolatey first
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 choco install -y                       ^
 google-chrome-x64                      ^
