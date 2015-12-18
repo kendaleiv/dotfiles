1b3bebe7f7de54b0d7c4df12125611dbf10e1104
Ken Dale <ken@kendaleiv.com>
2015-12-17 23:07:42 -0500

    Switch google-chrome-x64 Chocolatey package for googlechrome Chocolatey package

--- windows-setup/chocolatey-installs.cmd
+++ windows-setup/chocolatey-installs.cmd
@@ -3,7 +3,7 @@
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 
 choco install -y                       ^
-google-chrome-x64                      ^
+googlechrome                           ^
 keepass.install                        ^
 spotify                                ^
 7zip.commandline                       ^
