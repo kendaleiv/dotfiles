76fb89bef6af9bef2bdd32fcae64087c67cd0cdf
Ken Dale <ken@kendaleiv.com>
2017-07-01 09:51:27 -0400

    Remove gem update --system

--- windows-setup/post-software-installs-setup.cmd
+++ windows-setup/post-software-installs-setup.cmd
@@ -24,16 +24,6 @@ echo.
 echo ## Ruby
 echo.
 
-:: This isn't the best way to fix the SSL issue.
-:: It's not secure and requires interactively confirming adding an insecure source.
-:: But, it's convenient...
-:: http://stackoverflow.com/a/27447105
-call gem sources -r https://rubygems.org/
-call gem sources -a http://rubygems.org/
-call gem update --system
-call gem sources -r http://rubygems.org/
-call gem sources -a https://rubygems.org/
-
 call gem install bundler
 call gem install jekyll
 call gem install travis
