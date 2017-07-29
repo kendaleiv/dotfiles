0114bb66e803d0689384fa2adc0df58e62115d08
Ken Dale <ken@kendaleiv.com>
2017-07-28 23:47:25 -0400

    Remove some Chocolatey pinned packages

--- windows-setup/chocolatey-installs.cmd
+++ windows-setup/chocolatey-installs.cmd
@@ -47,17 +47,10 @@ winscp                                 ^
 wireshark                              ^
 sql-server-management-studio           ^
 
-::
-:: Pin software that automatically updates
-::
 choco pin add --name=docker-for-windows
-choco pin add --name=firefox
 choco pin add --name=googlechrome
-choco pin add --name=paint.net
-choco pin add --name=postman
 choco pin add --name=screenhero
 choco pin add --name=slack
-choco pin add --name=visualstudiocode
 
 choco install -y spotify
 choco pin add --name=spotify
