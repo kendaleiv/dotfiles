8e701920cdd339778f86a7701fb5cdd06ad147d4
Ken Dale <ken@kendaleiv.com>
2020-05-13 18:21:40 -0400

    Remove papercut Chocolatey package

--- windows-setup/chocolatey-installs.cmd
+++ windows-setup/chocolatey-installs.cmd
@@ -31,7 +31,6 @@ nuget.commandline                      ^
 nugetpackageexplorer                   ^
 paint.net                              ^
 poshgit                                ^
-papercut                               ^
 postman                                ^
 procexp                                ^
 putty                                  ^
