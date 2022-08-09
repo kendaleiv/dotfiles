3f8aa5ba3383cf35e613347bd611bd03b1bc9f09
Ken Dale <ken@kendaleiv.com>
2022-08-09 16:41:57 -0400

    Move to winget

--- README.md
+++ README.md
@@ -4,10 +4,9 @@ This is my dotfiles repo.
 
 ## windows-setup
 
-To setup a new Windows machine, execute the following files in the **windows-setup** folder in order **with Administrator permissions**:
+To setup a new Windows machine, execute the following files in the **windows-setup** folder in order:
 
-1. **chocolatey-installs.cmd**
-2. **chocolatey-installs-personal.cmd** *(optional, depending on machine being setup)*
+1. **winget-installs.cmd**
 3. **post-software-installs-setup.cmd**
 
 Now, manually perform the steps in **post-software-installs-manual-todo.md**.
---
+++ windows-setup/winget-installs.cmd
@@ -0,0 +1,21 @@
+winget install 7zip.7zip --exact --source winget
+winget install Git.Git --exact --source winget
+winget install GitHub.GitLFS --exact --source winget
+winget install Google.Chrome --exact --source winget
+winget install KDE.KDiff3 --exact --source winget
+winget install Microsoft.AzureCLI --exact --source winget
+winget install Microsoft.AzureDataStudio --exact --source winget
+winget install Microsoft.AzureStorageExplorer --exact --source winget
+winget install Microsoft.SQLServerManagementStudio --exact --source winget
+winget install Microsoft.VisualStudioCode --exact --source winget
+winget install Microsoft.WindowsTerminal --exact --source winget
+winget install Mozilla.Firefox --exact --source winget
+
+REM NuGet Package Explorer
+winget install 9WZDNCRDMDM3 --exact --source msstore
+
+winget install OpenJS.NodeJS --exact --source winget
+winget install Postman.Postman --exact --source winget
+winget install StefansTools.grepWin --exact --source winget
+winget install WinDirStat.WinDirStat --exact --source winget
+winget install WiresharkFoundation.Wireshark --exact --source winget
