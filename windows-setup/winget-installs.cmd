20b76a83f71ca7806f60e682bd8bb378a381e4c2
Ken Dale <ken@kendaleiv.com>
2024-01-13 20:08:02 -0500

    Remove items

--- windows-setup/winget-installs.cmd
+++ windows-setup/winget-installs.cmd
@@ -3,14 +3,10 @@ winget install GitHub.GitLFS --exact --source winget
 winget install Google.Chrome --exact --source winget
 winget install Microsoft.AzureCLI --exact --source winget
 winget install Microsoft.AzureDataStudio --exact --source winget
-winget install Microsoft.AzureStorageExplorer --exact --source winget
 winget install Microsoft.PowerShell --exact --source winget
 winget install Microsoft.SQLServerManagementStudio --exact --source winget
 winget install Microsoft.VisualStudioCode --exact --source winget
 winget install Mozilla.Firefox --exact --source winget
-
-REM NuGet Package Explorer
-winget install 9WZDNCRDMDM3 --exact --source msstore
 
 winget install OpenJS.NodeJS --exact --source winget
 winget install Postman.Postman --exact --source winget
